ada8f007e364259a6c47da710f6bfa947dbf250c
zc <zc@192.168.191.2>
2016-08-12 08:31:12 +0800

    8.12

--- src/com/phei/netty/protocol/udp/ChineseProverbClientHandler.java
+++ src/com/phei/netty/protocol/udp/ChineseProverbClientHandler.java
@@ -25,23 +25,21 @@ import io.netty.util.CharsetUtil;
  * @date 2014年2月14日
  * @version 1.0
  */
-public class ChineseProverbClientHandler extends
-	SimpleChannelInboundHandler<DatagramPacket> {
+public class ChineseProverbClientHandler extends SimpleChannelInboundHandler<DatagramPacket> {
 
-    @Override
-    public void messageReceived(ChannelHandlerContext ctx, DatagramPacket msg)
-	    throws Exception {
-	String response = msg.content().toString(CharsetUtil.UTF_8);
-	if (response.startsWith("谚语查询结果: ")) {
-	    System.out.println(response);
-	    ctx.close();
+	
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
+		String response = msg.content().toString(CharsetUtil.UTF_8);
+		if (response.startsWith("谚语查询结果: ")) {
+			System.out.println(response);
+			ctx.close();
+		}
 	}
-    }
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-	    throws Exception {
-	cause.printStackTrace();
-	ctx.close();
-    }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
 }
